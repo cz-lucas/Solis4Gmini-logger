02c4b0c1c584c85c4db3702670cb51829c83a1d1
10k-resistor <robotx2016@gmx.de>
2021-04-08 18:58:36 +0200

    Changed NTP implementation

--- src/time.h
+++ src/time.h
@@ -5,6 +5,7 @@ class timeControl{
     public:
     timeControl();
     void begin();
+    void loop();
     void print();
     void getTime(uint8_t *_hour, uint8_t *_minute);
     void getDate(uint16_t *_year, uint8_t *_month, uint8_t *_day);
